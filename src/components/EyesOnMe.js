a8da7aadb9896c0c77045bf21947b49f54104491
Dan Koskei <qimtaid@gmail.com>
2024-04-25 22:16:35 +0300

    complete lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,25 @@
 // Code EyesOnMe Component Here
+import React, {useState, useRef} from 'react';
+
+
+
+function EyesOnMe() {
+    const [focus, SetFocus] = useState(false);
+    const buttonRef = useRef();
+
+    const handleFocus = ()=> {
+        console.log('Good!');
+        SetFocus(true);
+    };
+
+    const handleBlur = () => {
+        console.log('Hey! Eyes on me!');
+        SetFocus(false);
+    }
+  return (
+    <button ref={buttonRef} onFocus={handleFocus} onBlur={handleBlur}>Eyes On Me</button>
+  )
+}
+
+export default EyesOnMe
+
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
+import React from "react";
+
 // Code Keypad Component Here
 
 function Keypad (){
+    const handleChange = (event)=> {
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <input type="password" onChange={handleChange}/>
+
+        
     )
 }
 
